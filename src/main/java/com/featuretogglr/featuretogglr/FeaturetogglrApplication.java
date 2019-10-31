d42325224bc14b281432d264b156fbc82d544e6f
Vijay Rangan <vijay.rangan@gmail.com>
2019-10-31 18:30:05 +0530

    Scaffold project

---
+++ src/main/java/com/featuretogglr/featuretogglr/FeaturetogglrApplication.java
@@ -0,0 +1,13 @@
+package com.featuretogglr.featuretogglr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturetogglrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaturetogglrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/featuretogglr/featuretogglr/FeaturetogglrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.featuretogglr.featuretogglr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeaturetogglrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
